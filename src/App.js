e0ea9887e8c75e4024cdb307847ae58f1116ece6
Winniee356 <lamwingjie@gmail.com>
2022-06-04 23:27:16 +0200

    Insert html base with bootstrap

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather App </h1>
         <Weather/>
       </div>
       <small>
--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,39 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import "./Weather.css";
 
-export default function WeatherSearch() {
-  let [city, setCity] = useState("");
-  let [load, setload] = useState(false);
-  let [weather, setWeather] = useState();
-
-  function showWeather(response) {
-    setload(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5d58dab7c52bc5b6821b6be77d25d60e`;
-    axios.get(url).then(showWeather);
-  }
-
-  function changeCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input type="search" onChange={changeCity} />
-      <input type="submit" value="Search" />
-    </form>
-  );
-
-  if (load) {
+export default function Weather() {
     return (
-      <div>
-        {form}
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+          <input type="search"placeholder="Enter a city" className="form-control"/>
+          </div>
+          <div className="col-3">
+          <input type="submit" value="Search" className="btn btn-secondary"/>
+          </div>
+          </div>
+        </form>
+      <h1>Amsterdam</h1>
         <ul>
           <li>
-            Temperature: {Math.round(weather.temperature)}°C
+           Saturday 18:00
           </li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
-          <li>
-            <img
-              src={weather.icon}
-              alt={weather.description}
-            />
-          </li>
+          <li>Sunny</li>
         </ul>
+        <div className="row">
+          <div className="col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny"/>
+            23°C
+          </div>
+          <div className="col-6">
+          <ul>
+              <li>Precipitation: 5%</li>
+              <li>Humidity:10%</li>
+              <li>Wind: 11 km/h</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
-  } else {
-    return form;
-  }
 }
