05e6aa4c84947928dde173afe2e3f41e3183d979
Winniee356 <lamwingjie@gmail.com>
2022-06-09 19:30:11 +0200

    Added Weather information component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
@@ -33,27 +33,7 @@ if (weatherData.ready) {
           </div>
           </div>
         </form>
-      <h1>{weatherData.city}</h1>
-        <ul>
-          <li> <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            < div className="clearfix mt-3">
-            <img src={weatherData.iconUrl} alt={weatherData.description}className="float-left"/>
-                <span className="temperature">{Math.round(weatherData.temperature)}</span>
-                <span className="unit">°C</span>
-            </div>
-          </div>
-          <div className="col-6">
-          <ul>
-              <li>Humidity:{weatherData.humidity}%</li>
-              <li>Wind:{weatherData.wind} km/h</li>
-            </ul>
-          </div>
-        </div>
+      <WeatherInfo data={weatherData}/>
       </div>
     );
   } else {
---
+++ src/WeatherInfo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+    return (
+        <div className="WeatherInfo">
+    <h1>{props.data.city}</h1>
+    <ul>
+      <li> <FormattedDate date={props.data.date} />
+      </li>
+      <li className="text-capitalize">{props.data.description}</li>
+    </ul>
+    <div className="row">
+      <div className="col-6">
+        < div className="clearfix mt-3">
+        <img src={props.data.iconUrl} alt={props.data.description}className="float-left"/>
+            <span className="temperature">{Math.round(props.data.temperature)}</span>
+            <span className="unit">°C</span>
+        </div>
+      </div>
+      <div className="col-6">
+      <ul>
+          <li>Humidity:{props.data.humidity}%</li>
+          <li>Wind:{props.data.wind} km/h</li>
+        </ul>
+      </div>
+    </div>
+    </div>
+    );
+}
