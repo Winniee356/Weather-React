f82a7ce4fdf3a7f23889cf02ec538edf177aa6d4
Winniee356 <lamwingjie@gmail.com>
2022-06-13 22:34:19 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,16 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast() {
-    return "This is the forecast";
+    return (
+        <div className="WeatherForecast">
+            <div className="row">
+                <div className="col">
+                Tue
+                <WeatherIcon code="01d" />
+                21/12
+                </div>
+            </div>
+        </div>
+    );
 }
