5bdb55cb3de3af35b991920fd239dfb0d5903cc0
Winniee356 <lamwingjie@gmail.com>
2022-06-13 22:16:37 +0200

    Create WeatherForecast component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -51,6 +52,7 @@ if (weatherData.ready) {
           </div>
         </form>
       <WeatherInfo data={weatherData}/>
+      <WeatherForecast />
       </div>
     );
   } else {
