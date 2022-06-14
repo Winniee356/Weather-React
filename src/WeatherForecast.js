fafedd0015947fb27a3b7e050e167e41b1dcd711
Winniee356 <lamwingjie@gmail.com>
2022-06-14 16:35:29 +0200

    Update WeatherForecast css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,6 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
     return (
@@ -9,8 +10,8 @@ export default function WeatherForecast() {
                 <div className="WeatherForecast-day">Tue</div>
                 <WeatherIcon code="01d" />
                 <div className="WeatherForecast-temperatures">
-                    <span className="WeatherForecast-temperature-max">21</span>/
-                    <span className="WeatherForecast-temperature-min">12</span>
+                    <span className="WeatherForecast-temperature-max">21°</span>/
+                    <span className="WeatherForecast-temperature-min">12°</span>
                     </div>
                 </div>
             </div>
