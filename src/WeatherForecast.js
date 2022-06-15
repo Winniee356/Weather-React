1b579ec667074afcc9aa5fdfa38c58cff536ac01
Winniee356 <lamwingjie@gmail.com>
2022-06-15 21:38:39 +0200

    Start intergrated API for Weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast() {
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
+    let apiKey = "5d58dab7c52bc5b6821b6be77d25d60e";
+    let longtitude = 4.9;
+    let latitude = 52.3;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=${apiKey}`
+
+    axios.get(apiUrl).then(handleResponse);
+
     return (
         <div className="WeatherForecast">
             <div className="row">
