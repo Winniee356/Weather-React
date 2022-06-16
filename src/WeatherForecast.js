b0f7b06595fae96c5a0869bf144dc79f82e905db
Winniee356 <lamwingjie@gmail.com>
2022-06-16 23:23:02 +0200

    round up degrees

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -22,8 +22,8 @@ export default function WeatherForecast(props) {
                     <div className="WeatherForecast-day">{forecast[0].dt}</div>
                     <WeatherIcon code={forecast[0].weather[0].icon} size={36}/>
                     <div className="WeatherForecast-temperatures">
-                        <span className="WeatherForecast-temperature-max">{forecast[0].temp.max}°</span>/
-                        <span className="WeatherForecast-temperature-min">{forecast[0].temp.min}°</span>
+                        <span className="WeatherForecast-temperature-max">{Math.round(forecast[0].temp.max)}°</span>/
+                        <span className="WeatherForecast-temperature-min">{Math.round(forecast[0].temp.min)}°</span>
                         </div>
                     </div>
                 </div>
