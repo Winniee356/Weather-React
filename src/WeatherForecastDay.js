9efa139cb94e94aacae440c26e416cd0ba9a8d5e
Winniee356 <lamwingjie@gmail.com>
2022-06-17 15:54:01 +0200

    Show Day of the week

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -12,9 +12,17 @@ export default function WeatherForecastDay(props) {
         return `${temperature}°`;
     }
 
+    function day () {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        return days[day];
+    }
+
     return (
         <div>
-            <div className="WeatherForecast-day">{props.data.dt}</div>
+            <div className="WeatherForecast-day">{day()}</div>
                     <WeatherIcon code={props.data.weather[0].icon} size={36}/>
                     <div className="WeatherForecast-temperatures">
                         <span className="WeatherForecast-temperature-max">{maxTemperature()}</span>/
